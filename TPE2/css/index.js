7396a69bc2391481dd3084dc898d8093ca418998
Malinowsk <juanrago2015@gmail.com>
2024-09-25 00:32:49 -0300

    Primer versión del registro

---
+++ TPE2/css/index.js
@@ -0,0 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+const registro = document.querySelector(".registro");
+const registroP1 = document.querySelector(".registro-p1");
+const registroP2 = document.querySelector(".registro-p2");
+const registroP3 = document.querySelector(".registro-p3");
+
+let translateValue = 0;
+const btnRegistro = document.querySelector("#btn-registro").addEventListener("click", ()=>{
+    transladar(-100);
+});
+const btnInicioSesion = document.querySelector("#btn-inicio-sesion").addEventListener("click", ()=>{
+    transladar(100);
+});
+
+function transladar(valor){
+    translateValue += valor;
+    loginForm.style.transform=`translateX(${translateValue}%)`
+    registro.style.transform=`translateX(${translateValue}%)`
+    registroP1.style.transform=`translateX(${translateValue}%)`
+    registroP2.style.transform=`translateX(${translateValue}%)`
+    registroP3.style.transform=`translateX(${translateValue}%)`
+}
