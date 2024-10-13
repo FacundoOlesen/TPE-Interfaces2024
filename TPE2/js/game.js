7facff5c2177239ca9945c820b40ebab78262e7c
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2024-10-13 15:28:08 -0300

    Acomodo menu hamburgesa home

--- TPE2/js/game.js
+++ TPE2/js/game.js
@@ -3,21 +3,27 @@ const abrir = document.querySelector(".hmb")
 const body = document.querySelector("body")
 const menu = document.querySelector(".menu-desp")
 const main = document.querySelector(".main")
+const mainhome = document.querySelector(".main-home")
 
 const avatar = document.querySelector(".avatar")
 const profile = document.querySelector(".profile")
 const cj = document.querySelector(".cj")
 const x = document.querySelector(".x")
+const open = document.querySelector(".menu-hamburguesa")
 
 
 abrir.addEventListener("click", () => {
     menu.classList.toggle("hide")
-    body.classList.toggle("blur")
     body.classList.toggle("overflow")
     main.classList.toggle("active")
     main.classList.toggle("main")
     main.classList.toggle("main-helper")
-    console.log("hola")
+})
+
+
+open.addEventListener("click", () => {
+    mainhome.classList.toggle("active")
+    mainhome.classList.toggle("main-helper2")
 })
 
 
