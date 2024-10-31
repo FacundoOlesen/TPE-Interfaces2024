6ed1c7d5a738cfb85484190192cd26cf16d4ac6c
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2024-10-31 17:03:35 -0300

    Mejoras en el codigo

--- TPE3/js/4enLinea.js
+++ TPE3/js/4enLinea.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let offsetX = 158;
     let offsetY = 55;
 
-    let inGame= false
+    let inGame = false
 
     let btns = []
 
@@ -39,25 +39,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const ctx = canvas.getContext('2d');
 
 
-        
-        
-      
+
         dibujarBienvenidaJuego()
         function dibujarBienvenidaJuego() {
             let fondoJuego = new Image();
             fondoJuego.src = "./img/fondotablero.jpg";
-    
+
             fondoJuego.onload = function () {
                 ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    
+
                 ctx.drawImage(fondoJuego, 0, 0, ctx.canvas.width, ctx.canvas.height);
-                ctx.textAlign='center'
-    
-                
+                ctx.textAlign = 'center'
+
                 let btn4Enlinea = new Boton(ctx, 300, 40, "4 en línea", 200, 60, 200 / 4, 60 / 1.5)
                 btn4Enlinea.dibujar()
                 btns.push(btn4Enlinea)
-    
+
                 let btn5Enlinea = new Boton(ctx, 300, 140, "5 en línea", 200, 60, 200 / 4, 60 / 1.5)
                 btn5Enlinea.dibujar()
                 btns.push(btn5Enlinea)
@@ -69,117 +66,112 @@ document.addEventListener('DOMContentLoaded', () => {
                 let btn7Enlinea = new Boton(ctx, 300, 340, "7 en línea", 200, 60, 200 / 4, 60 / 1.5)
                 btn7Enlinea.dibujar()
                 btns.push(btn7Enlinea)
-                console.log(btns)
             };
         }
-       
-       
-       
-       
-        //CUANDO YA ELIGIO MODO: 
 
-       
-    
+
+
+
         function drawFigures() {
             for (let i = 0; i < tablero.arrFichas.length; i++) {
                 tablero.arrFichas[i].draw();
             }
         }
-    
+
         function onMouseDown(e) {
-            console.log(e)
-            if(!inGame){
-                console.log("posX: "+e.clientX)
-                console.log("posY: "+ e.clientY)
-                for (let i = 0; i < btns.length; i++) {
-                    const element = btns[i];
+            if (!inGame) {
                     let rect = ctx.canvas.getBoundingClientRect();
                     let canvasX = e.clientX - rect.left;
                     let canvasY = e.clientY - rect.top;
-                    if (element.isPointInside(canvasX, canvasY)) {
-                        console.log(element)
-                        let modo = element.getTextoBoton()[0]
-                        console.log(modo)
-                        switch (modo) {
-                            case '5':
-                                columnas = 8;
-                                filas = 7;
-                                fichaRadio = 24;
-                                espColumnas = 68.5;
-                                espFilas = 74.5;
-                                offsetX = 138;
-                                offsetY = 46;
-                                break;
-                    
-                            case '6':
-                                columnas = 9;
-                                filas = 8;
-                                fichaRadio = 22;
-                                espColumnas = 61.5;
-                                espFilas = 63.5;
-                                offsetX = 146;
-                                offsetY = 37;
-                                break;
-                    
-                            case '7':
-                                columnas = 10;
-                                filas = 7;
-                                fichaRadio = 20.5;
-                                espColumnas = 65.5;
-                                espFilas = 62.5;
-                                offsetX = 120;
-                                offsetY = 53;
-                                break;
-                    
-                            default:
-                                console.log("Modo no válido");
-                                break;
-                        }
-                    
+                    let clickedMode = findClickedFigure(canvasX, canvasY, btns)
+                    if (clickedMode) {
+                        let modo = clickedMode.getTextoBoton()[0]
+                        elegirModo(modo)
                         tablero = new Tablero(ctx, columnas, filas, fichaRadio, espFilas, espColumnas, offsetX, offsetY);
                         tablero.cargarFichas(ctx);
-                        inGame=true
-                        return element;
+                        inGame = true
                     }
-                }
+               
             }
-            else{
+
+
+            else {
                 e.preventDefault();
                 isMouseDown = true;
                 if (lastClickedFigure) lastClickedFigure = null;
-        
-                let clickFig = findClickedFigure(e.clientX, e.clientY);
-                if (clickFig) lastClickedFigure = clickFig;
+
+                let clickFig = findClickedFigure(e.clientX, e.clientY, tablero.arrFichas);
+                if (clickFig) 
+                    lastClickedFigure = clickFig;
             }
         }
-    
+
         function onMouseMove(e) {
             e.preventDefault();
             if (isMouseDown && lastClickedFigure) {
                 let rect = tablero.ctx.canvas.getBoundingClientRect();
                 let canvasX = e.clientX - rect.left;
                 let canvasY = e.clientY - rect.top;
-    
+
                 lastClickedFigure.setPos(canvasX, canvasY);
                 tablero.dibujarTablero();
                 drawFigures();
             }
         }
-    
+
         function onMouseUp(e) {
             e.preventDefault();
             isMouseDown = false;
         }
-    
-        function findClickedFigure(x, y) {
-            for (let i = 0; i < tablero.arrFichas.length; i++) {
-                const element = tablero.arrFichas[i];
-                if (element.isPointInside(x, y)) return element;
+
+        function findClickedFigure(x, y, arr) {
+            for (let i = 0; i < arr.length; i++) {
+                const element = arr[i];
+                if (element.isPointInside(x, y))
+                     return element;
             }
         }
-    
+
+        function elegirModo(modo){
+            switch (modo) {
+                case '5':
+                    columnas = 8;
+                    filas = 7;
+                    fichaRadio = 24;
+                    espColumnas = 68.5;
+                    espFilas = 74.5;
+                    offsetX = 138;
+                    offsetY = 46;
+                    break;
+
+                case '6':
+                    columnas = 9;
+                    filas = 8;
+                    fichaRadio = 22;
+                    espColumnas = 61.5;
+                    espFilas = 63.5;
+                    offsetX = 146;
+                    offsetY = 37;
+                    break;
+
+                case '7':
+                    columnas = 10;
+                    filas = 7;
+                    fichaRadio = 20.5;
+                    espColumnas = 65.5;
+                    espFilas = 62.5;
+                    offsetX = 120;
+                    offsetY = 53;
+                    break;
+
+                default:
+                    console.log("Modo no válido");
+                    break;
+            }
+        }
+
         canvas.addEventListener('mousedown', onMouseDown, false);
         canvas.addEventListener('mousemove', onMouseMove, false);
         canvas.addEventListener('mouseup', onMouseUp, false);
     });
-    });
+});
