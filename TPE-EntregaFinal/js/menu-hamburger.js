def96f879ba529cdf4d587421a8b90a3eec00e9d
Malinowsk <juanrago2015@gmail.com>
2024-11-14 12:19:35 -0300

    Agrego lógica para animacion barra de nav

--- TPE-EntregaFinal/js/menu-hamburger.js
+++ TPE-EntregaFinal/js/menu-hamburger.js
@@ -1,5 +1,9 @@
 const hamburgerButton = document.getElementById("hamburger-button");
+const barraDeNav = document.getElementById("nav");
+const header = document.getElementById("header");
 
 hamburgerButton.addEventListener("click", function () {
     hamburgerButton.classList.toggle("transformed");
+    barraDeNav.classList.toggle("visible");
+    header.classList.toggle("visible");
 });
