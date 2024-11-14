be5729e920867f64f67f9f958f0447f42bc2fa64
Malinowsk <juanrago2015@gmail.com>
2024-11-14 02:14:19 -0300

    Agrego lógica para animación menu hamburger

---
+++ TPE-EntregaFinal/js/menu-hamburger.js
@@ -0,0 +1,5 @@
+const hamburgerButton = document.getElementById("hamburger-button");
+
+hamburgerButton.addEventListener("click", function () {
+    hamburgerButton.classList.toggle("transformed");
+});
