d87afd38f8f9b7ed991064d66c75537ab39c7a51
Malinowsk <juanrago2015@gmail.com>
2024-11-14 23:11:36 -0300

    agrego funcionalidad de parallax del primer fondo

---
+++ TPE-EntregaFinal/js/parallax.js
@@ -0,0 +1,47 @@
+
+const containerMain = document.getElementById("container-main");
+
+const seccionArbolIzq = document.getElementById("seccion-arbol-izq");
+const seccionArbolDer = document.getElementById("seccion-arbol-der");
+const seccionArbolDerChico = document.getElementById("seccion-arbol-der-chico");
+
+const arbusto1 = document.getElementById("arbusto1");
+const arbusto2 = document.getElementById("arbusto2");
+const arbusto3 = document.getElementById("arbusto3");
+const arbusto4 = document.getElementById("arbusto4");
+
+const piedra1 = document.getElementById("piedra1");
+const piedra2 = document.getElementById("piedra2");
+const piedra3 = document.getElementById("piedra3");
+const piedra4 = document.getElementById("piedra4");
+
+const elipse1 = document.getElementById("elipse1");
+const elipse2 = document.getElementById("elipse2");
+const elipse3 = document.getElementById("elipse3");
+
+const blockRojo1 = document.getElementById("block-rojo-1");
+const blockNaranja2 = document.getElementById("block-naranja-2");
+const blockAmarillo3 = document.getElementById("block-amarillo-3");
+
+containerMain.onscroll = function () {
+  let y = containerMain.scrollTop;
+  arbusto1.style.left = 200 + y * 0.05 + "px";
+  arbusto2.style.left = 120 + y * 0.05 + "px";
+  arbusto3.style.right = 0 - y * 0.05 + "px";
+  arbusto4.style.right = 60 - y * 0.05 + "px";
+  piedra1.style.left = 130 + y * 0.05 + "px";
+  piedra2.style.right = 170 - y * 0.05 + "px";
+  piedra3.style.right = 90 - y * 0.05 + "px";
+  piedra4.style.right = 170 - y * 0.05 + "px";
+  seccionArbolIzq.style.left = 0 + y * 0.05 + "px";
+  seccionArbolDer.style.right = 0 - y * 0.05 + "px";
+  seccionArbolDerChico.style.right = 0 - y * 0.05 + "px";
+
+  blockRojo1.style.bottom = 130 + y * 0.2 + "px";
+  blockNaranja2.style.bottom = 275 - y * 0.2 + "px";
+  blockAmarillo3.style.top = 323 - y * 0.2 + "px";
+  blockAmarillo3.style.left = 710.25 + y * 0.2 + "px";
+
+  elipse2.style.bottom = 176 - y * 0.05 + "px";
+  elipse3.style.right = 380 - y * 0.2 + "px";
+};
