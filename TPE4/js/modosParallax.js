51766e22e74bffff9361c121d1b3153110c82a37
Enzo Jatip <enzojatip@gmail.com>
2024-11-19 13:50:22 -0300

    Mejoras en la seccion modos

---
+++ TPE4/js/modosParallax.js
@@ -0,0 +1,32 @@
+let img = document.getElementById("modo-img");
+
+// Array con las rutas de las imágenes que se cambiarán
+const imagenes = [
+    "./img/figura0.png",
+    "./img/figura1.png",
+    "./img/figura2.png",
+    "./img/figura3.png",
+    "./img/figura4.png",
+    "./img/figura5.png",
+    "./img/figura6.png",
+    "./img/figura7.png",
+    "./img/figura8.png",
+];
+
+window.addEventListener("scroll", function () {
+    const scrollY = window.scrollY;
+
+    if (scrollY >= 3950 && scrollY <= 7200) {
+        img.classList.add("stickyModo");
+        img.classList.remove("sticky-final", "sticky-inicial");
+        const index = Math.floor((scrollY - 3950) / 400) % imagenes.length;
+        img.src = imagenes[index];
+
+    } else if (scrollY > 7200) {
+        img.classList.add("sticky-final");
+        img.classList.remove("stickyModo", "sticky-inicial");
+    } else {
+        img.classList.add("sticky-inicial");
+        img.classList.remove("stickyModo", "sticky-final");
+    }
+});
