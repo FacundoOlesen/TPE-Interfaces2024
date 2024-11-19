2f70e867c55dc02a796876e45cd619273c9a935d
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2024-11-19 12:36:46 -0300

    mejoras en sticky header

--- TPE4/js/stickyHeader.js
+++ TPE4/js/stickyHeader.js
@@ -1,6 +1,7 @@
 let stickyHeader = document.getElementById("header")
+let nav = document.getElementById("nav")
 
 window.addEventListener("scroll", function () {
-    console.log(window.scrollY)
     stickyHeader.classList.toggle("sticky", window.scrollY > 0)
+    nav.classList.toggle("hide",  window.scrollY > 0)
 })
