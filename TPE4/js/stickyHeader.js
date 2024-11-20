b73b39d3748b5b0742601f7a48e7ec4e2e3cc7d4
Enzo Jatip <enzojatip@gmail.com>
2024-11-20 16:10:16 -0300

    Correccion de velocidad al hacerse sticky

--- TPE4/js/stickyHeader.js
+++ TPE4/js/stickyHeader.js
@@ -8,17 +8,17 @@ const minHeight = 55;
 
 window.addEventListener("scroll", () => {
     let y = window.scrollY;
-    if (window.scrollY <= 879) {
+    if (window.scrollY <= 179) {
         logo.style.marginTop = 0 + y + "px";
-        let newWidth = Math.max(minWidth, initialWidth - y * 0.5);
-        let newHeight = Math.max(minHeight, initialHeight - y * 0.25);
+        let newWidth = Math.max(minWidth, initialWidth - y * 1.75);
+        let newHeight = Math.max(minHeight, initialHeight - y * 1.25);
 
         logo.style.width = newWidth + "px";
         logo.style.height = newHeight + "px";
 
-        window.scrollY >= 847 ? logo.classList.add("fixed") : logo.classList.remove("fixed")
+        window.scrollY >= 179 ? logo.classList.add("fixed") : logo.classList.remove("fixed")
     }
-     if (window.scrollY >= 845) {
+     if (window.scrollY >= 179) {
         logo.classList.add("fixed")
         logo.style.width = 131.5 + "px";
         logo.style.height = 105.75 + "px";
