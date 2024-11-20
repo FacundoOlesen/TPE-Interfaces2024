b83134d62943e1ed1cc756444e7cb424f52fd564
Facundo Olesen <faolesen@alumnos.exa.unicen.edu.ar>
2024-11-20 15:56:22 -0300

    arreglo problema logo al scrollear

--- TPE4/js/stickyHeader.js
+++ TPE4/js/stickyHeader.js
@@ -8,7 +8,6 @@ const minHeight = 55;
 
 window.addEventListener("scroll", () => {
     let y = window.scrollY;
-    console.log(y)
     if (window.scrollY <= 879) {
         logo.style.marginTop = 0 + y + "px";
         let newWidth = Math.max(minWidth, initialWidth - y * 0.5);
@@ -19,12 +18,16 @@ window.addEventListener("scroll", () => {
 
         window.scrollY >= 847 ? logo.classList.add("fixed") : logo.classList.remove("fixed")
     }
-    else if (window.scrollY >= 845) {
+     if (window.scrollY >= 845) {
         logo.classList.add("fixed")
         logo.style.width = 131.5 + "px";
         logo.style.height = 105.75 + "px";
-        logo.style.marginTop = -5  + "px";
+        logo.style.marginTop = -5 + "px";
 
     }
 });
+
+window.addEventListener("click",(e) =>{
+    console.log(e.offsetX)
+})
 
