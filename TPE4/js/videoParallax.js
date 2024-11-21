4489f9e6f78c22a411d43f9a4712753d67fc337a
Enzo Jatip <enzojatip@gmail.com>
2024-11-21 15:08:37 -0300

    Correccion en seccion video, para darle movimiento al video

--- TPE4/js/videoParallax.js
+++ TPE4/js/videoParallax.js
@@ -1,6 +1,7 @@
 window.addEventListener("scroll", () => {
     const seccion = document.querySelector(".seccion-video");
     const img = document.querySelector(".parallax-img");
+    const video=document.querySelector(".video-container");
 
     const seccionTop = seccion.offsetTop;
     const seccionAltura = seccion.offsetHeight;
@@ -9,5 +10,6 @@ window.addEventListener("scroll", () => {
     if (scrollY >= seccionTop && scrollY <= seccionTop + seccionAltura) {
         const posicionScroll = scrollY - seccionTop; 
         img.style.transform = `translateY(${-50 - posicionScroll * 0.3}px)`;
+        video.style.transform=`translateX(${50 - posicionScroll * 0.1}px)`;
     }
 });
